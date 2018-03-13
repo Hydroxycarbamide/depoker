1dd6edceb806d6ea4c73513fb44f63b44ee043f3
Hydroxycarbamide <nguyen.huang.eric@gmail.com>
2018-03-13 17:51:55 +0100

    Send p1 p2 names to Accueil Layout elements changed

--- app/src/main/java/com/cabanettes/axel/nguyen/eric/depoker/Accueil.java
+++ app/src/main/java/com/cabanettes/axel/nguyen/eric/depoker/Accueil.java
@@ -1,29 +1,22 @@
 package com.cabanettes.axel.nguyen.eric.depoker;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class Accueil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_accueil);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+        Intent intent = getIntent();
+        String player1 = intent.getStringExtra(MainActivity.PLAYER1);
+        String player2 = intent.getStringExtra(MainActivity.PLAYER2);
+
     }
+
+
 
 }
--- app/src/main/java/com/cabanettes/axel/nguyen/eric/depoker/MainActivity.java
+++ app/src/main/java/com/cabanettes/axel/nguyen/eric/depoker/MainActivity.java
@@ -1,13 +1,41 @@
 package com.cabanettes.axel.nguyen.eric.depoker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String PLAYER1 = "com.example.myfirstapp.PLAYER1";
+    public static final String PLAYER2 = "com.example.myfirstapp.PLAYER2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerPlayers(view);
+            }
+        });
+    }
+
+    protected void registerPlayers(View view){
+        Intent intent = new Intent(this, Accueil.class);
+        intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
+        EditText joueur1 = (EditText) findViewById(R.id.editTextPlayer1);
+        EditText joueur2 = (EditText) findViewById(R.id.editTextPlayer2);
+        String nomJoueur1 = joueur1.getText().toString();
+        String nomJoueur2 = joueur2.getText().toString();
+        intent.putExtra(PLAYER1, nomJoueur1);
+        intent.putExtra(PLAYER2, nomJoueur2);
+        startActivity(intent);
+
+
     }
 }
