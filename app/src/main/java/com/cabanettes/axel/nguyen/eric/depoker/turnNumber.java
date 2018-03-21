a3bf0cf5b89e2ff5d3ba91ce9ff3ba8041a9ef07
Hydroxycarbamide <nguyen.huang.eric@gmail.com>
2018-03-21 22:52:04 +0100

    Added settings for the number of turns

--- app/src/main/java/com/cabanettes/axel/nguyen/eric/depoker/turnNumber.java
+++ app/src/main/java/com/cabanettes/axel/nguyen/eric/depoker/turnNumber.java
@@ -1,11 +1,16 @@
 package com.cabanettes.axel.nguyen.eric.depoker;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class turnNumber extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,41 @@ public class turnNumber extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_turn_number);
 
+        //Gestion du groupe radio de nombre de tours
 
+
+        Button btnOk = (Button) findViewById(R.id.btnTurnOK);
+        final RadioGroup radioTurnGroup = (RadioGroup) findViewById(R.id.radioTurnGroup);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                int id = radioTurnGroup.getCheckedRadioButtonId();
+                RadioButton radioButton = (RadioButton) findViewById(id);
+                int value = 1;
+                switch (radioButton.getText().toString()){
+                    case "1":
+                        value = 1;
+                        break;
+                    case "2":
+                        value = 2;
+                        break;
+                    case "3":
+                        value = 3;
+                        break;
+                }
+                changeValue(value);
+                finish();
+            }
+        });
+
+
+    }
+
+    //Change la valeur de turnNumber  dans /data/data/com.cabanettes.axel.nguyen.eric.depoker/shared_prefs/prefs.xml
+    private void changeValue(int value) {
+        SharedPreferences.Editor editor = getSharedPreferences("prefs", MODE_PRIVATE).edit();
+        editor.putInt("turnNumber", value);
+        editor.commit();
     }
 }
